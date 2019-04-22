3494430650ebf32830ef5de70d43ec45c994139e
Anton <antonliashok@gmail.com>
2019-04-22 18:38:01 +0300

    add: graphql-request

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { withStyles } from "@material-ui/core/styles";
 import { GoogleLogin } from 'react-google-login';
+import { GraphQLClient } from 'graphql-request';
 // import Typography from "@material-ui/core/Typography";
 
 const clientId='610206240128-8v62hvorsentppdeva5366e415ke30j8.apps.googleusercontent.com';
@@ -10,7 +11,9 @@ const Login = ({ classes }) => {
   const onSuccess = success => {
     const token = success.getAuthResponse().id_token;
 
-    console.log({ token });
+    const client = new GraphQLClient('http://localhost:4000/', {
+      headers: { authorization: token }
+    })
   };
 
   return <>
